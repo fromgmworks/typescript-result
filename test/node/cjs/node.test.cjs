58a6cffb6112c1fde4979483fc5f3ce0f3bff2cd
Gabe <gabemeola@gmail.com>
2022-11-06 15:26:43 -0700

    feat: version 3.4.0
    * chore: clean up
    
    * chore: adding in support for node esm
    
    * feat: switch to using rollup to bundling
    
    * test: add additional tests for match
    
    * chore: fix typo
    
    * chore: clean up
    
    * chore: update readme
    
    * chore: bump version

---
+++ test/node/cjs/node.test.cjs
@@ -0,0 +1,8 @@
+const AllResults = require('@utx/result')
+const AllRxjs = require('@utx/result/rxjs-operators');
+
+
+
+console.log('✨ Successfully imported All imports in CJS mode.', {
+    imports: [AllResults, AllRxjs]
+});
